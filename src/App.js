f26e3d72131a770c73df164b14d3a2cf6842d912
Farasat Ali <63093876+farasatAli1@users.noreply.github.com>
2020-09-17 23:44:21 +0500

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React from "react";
+import { Switch, Route, /*Link*/ } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
+// const HomePage = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <Link to='/topic'>Topics</Link>
+//       <button onClick={() => props.history.push('/topic')}>Topics</button>
+//       <h1>Home Page</h1>
+//     </div>
+//   );
+// };
+
+// const TopicList = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <h1>Topic List Page</h1>
+//     </div>
+//   );
+// };
+
+// const TopicDetail = (props) => {
+//   console.log(props)
+//   console.log(props.match.params.topicId)
+//   return (
+//     <div>
+//       <h1>Topic Detail Page</h1>
+//     </div>
+//   );
+// };
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>  {/* It says that as long as one route matches it renders it */}
+        <Route exact path="/" component={HomePage} />  {/* exact means show this page if and only if the url is exactly / */}
+        <Route exact path="/shop/hats" component={HatsPage} />
+        {/* <Route exact path="/topic/:topicId" component={TopicDetail} />  : means that we can put here dynamic path */}
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
 import "./menu-item.style.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div
         className="background-image"
         style={{
@@ -18,4 +19,5 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+// This withRouter will return MenuItem with access to history and match props
+export default withRouter(MenuItem);
