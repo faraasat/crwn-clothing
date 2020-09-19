a593edb19c6cc21885dcf715780876d7f77751ab
Farasat Ali <63093876+farasatAli1@users.noreply.github.com>
2020-09-19 08:59:16 +0500

    Updated ShopPage CollectionPreview and Added PreviewItem

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { Switch, Route, /*Link*/ } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = (props) => {
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+// const HatsPage = (props) => {
+//   return (
+//     <div>
+//       <h1>Hats Page</h1>
+//     </div>
+//   );
+// };
 
 // const HomePage = (props) => {
 //   console.log(props)
@@ -47,7 +48,7 @@ function App() {
     <div>
       <Switch>  {/* It says that as long as one route matches it renders it */}
         <Route exact path="/" component={HomePage} />  {/* exact means show this page if and only if the url is exactly / */}
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
         {/* <Route exact path="/topic/:topicId" component={TopicDetail} />  : means that we can put here dynamic path */}
       </Switch>
     </div>
