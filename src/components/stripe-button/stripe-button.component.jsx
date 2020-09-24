b573e58290b8dc31b92c2717957a5197ef0c70c5
Farasat Ali <63093876+farasatAli1@users.noreply.github.com>
2020-09-24 23:38:09 +0500

    Integrated Stripe and Change favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HLMkMBTKC4Y4y7weyEy8weAskV6imPJex5R1cF2jA8nIQFk1DP0HIzkcF5smNmQIgtr1SsQrEgqocTrjrEXTf2n00MoV62BTQ";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your Total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
